f0f35dd2c3a75f37a577bb8de57ad043c0ad7457
Natasari <penitasari93@gmail.com>
2015-05-11 23:54:28 +0700

    logout session

--- Front End/index.js
+++ Front End/index.js
@@ -17,19 +17,13 @@ var connection = mysql.createConnection({
  });
 connection.connect();
 
+var sess;
+var flag  = 1;
 
 var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 app.use(passport.initialize());
 app.use(passport.session());
-// username, password => inputan user dari form
-    // done => sebuah fungsi callback, kalau login cocok, argumen kedua isinya data user
-    //         kalau nggak, argumen kedua isinya "false"
-    //    if(username == "admin" && password == "admin123") {
-    //        done(null, {username: "admin"})
-    //
-    //    } else {
-    //        done(null, false, {message: "User atau password salah"});
-    //    }
+
 passport.use(new LocalStrategy(function(username, password, done) 
     {
         connection.query("SELECT * FROM user WHERE username='" + username + "'", function(err, rows, fields) 
@@ -37,6 +31,7 @@ passport.use(new LocalStrategy(function(username, password, done)
           if(rows.length > 0) {
             if(rows[0].password == password) {
                 done(null, {username: username});
+                
             } else {
                 done(null, false);
             }
@@ -59,87 +54,103 @@ passport.deserializeUser(function(user, done) {
 });
 
 
-app.get('/', function(req, res) {
-    res.redirect('/home');
-});
 
+
+
+/*---------------------------------------------------------------------------------------*/
+/*------------------------------------FUNGSI---------------------------------------------*/
+
+var cekAutentikasi = function(req, res, done) {
+    if(req.isAuthenticated()) {
+        done();
+    } else {
+        res.redirect('/home');
+    }
+}
+
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------GET---------------------------------------------*/
+
+app.get('/', cekAutentikasi, function(req, res) {
+    res.render("dashboard");
+});
 
 app.get('/register', function(req, res) {
-    res.render('register');
+    if(req.isAuthenticated()){
+	res.redirect('/dashboard');
+    }
+    else{
+	res.render('register');
+    }
 });
 
-/*
-app.post('/register', function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    connection.query("INSERT INTO user VALUES ('" + username + "','" + password + "')", function(err, rows, fields) {
-        res.end(username + " sudah didaftarkan. Silahkan login");
-    });
 
+app.get('/register#login', function(req,res){
+    res.write("aduuh nang ndi iki");
 });
-*/
-app.get('/login', function(req, res) {
-    res.render('login');
-});
-app.get('/dashboard', function(req, res) {
-    res.render('dashboard');
-});
+
+
+//--dari form login di dalam home.ejs
 app.get('/home', function(req, res) {
     res.render('home');
 });
-//app.post('/login', function(req, res) {
-//    // req.body => butuh body-parser buat mbacanya
-//    if(req.body.username=="admin" && req.body.password=="admin123") {
-//        // req.session => butuh express-session buat ngisinya
-//        req.session.is_login = true;
-//        req.session.username = "admin";
-//        res.redirect('/app');
-//    } else {
-//        res.redirect('/login');
-//    }
-//});
 
-app.post('/home', passport.authenticate('local', { successRedirect: '/app',failureRedirect: '/home',
-                                   failureFlash: false })
-);
+app.get('/plans', cekAutentikasi, function(req, res) {
+    res.render('plans', {username: req.user.username});
 
-app.get('/logout', function(req, res) {
+});
+
+app.get('/dashboard', cekAutentikasi, function(req, res) {
+    res.render('dashboard');
+});
+
+app.post('/logout', function (req, res){
+    if(req.isAuthenticated()){
+       req.logout();
+    }
+    res.end('done');
 });
 
 
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------POST--------------------------------------------*/
 
-//---------------------------------------------------------------------------------------------------------
+app.post('/home', passport.authenticate(
+   'local', { successRedirect: '/dashboard', failureRedirect: '/home', failureFlash: false })
+);
 
-//INI BUAT REGISTER
 app.post('/register', function(req, res) {
     var nama = req.body.nama;
     var email = req.body.email;
     var username = req.body.username;
     var password = req.body.password;
     
-   
     res.render('dashboard');
     connection.query("INSERT INTO user (nama, email, username, password) VALUES ('" + nama + "','"+ email +"', '"+ username +"', '"+ password +"')", 
         function(err, rows, fields){});
 });
 
 
-//---------------------------------------------------------------------------------------------------------
 
-var cekAutentikasi = function(req, res, done) {
-    if(req.isAuthenticated()) {
-        done();
-    } else {
-        res.redirect('/login');
+
+
+
+
+
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------BACKEND-----------------------------------------*/
+app.get('/app/create_user', cekAutentikasi, function(req, res) {
+console.log('flag: '+global.flag);
+    if(global.flag==1) {
+    	var ps = child_process.spawn('ssh', ['ripas@10.151.32.101', 'bash /etc/openvpn/easy-rsa/CreateUser.sh.old ' + req.user.username]);
+        global.flag = 2;
+	console.log('membuat user di 10.151.32.101');
     }
-}
-app.get('/app', cekAutentikasi,function(req, res) {
-    res.render('app', {username: req.user.username});
-});
-
-app.get('/app/inisiasi', cekAutentikasi, function(req, res) {
-    var ps = child_process.spawn('ssh', ['wira@192.168.56.103', 'sudo inisiasi.sh ' + req.user.username]);
-
+    else {
+	var ps = child_process.spawn('ssh', ['ripas@10.151.32.100', 'bash /etc/openvpn/easy-rsa/CreateUser.sh.old ' + req.user.username]);
+	console.log('membuat user di 10.151.32.100');
+        global.flag = 1;
+}	
     ps.on('exit', function(code) {
         if(code == 0) {
             res.end('Sukses');
@@ -150,6 +161,10 @@ app.get('/app/inisiasi', cekAutentikasi, function(req, res) {
 
     });
 
+});
+
+app.get('/downloadkey', function(req, res) {
+    res.download('/home/ripas/vpnClient/peni.ovpn', 'peni.ovpn'); // Set disposition and send it.
 });
 
 app.get('/app/listdir', cekAutentikasi, function(req, res) {
@@ -167,6 +182,5 @@ app.get('/app/listdir', cekAutentikasi, function(req, res) {
         res.render('listdir', {dir: hasil});
     });
 });
-
 
 app.listen(4444);
